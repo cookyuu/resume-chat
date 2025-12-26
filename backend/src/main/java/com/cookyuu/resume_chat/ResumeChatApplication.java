f3e6289a9d37b39d8e4abdebc875ddd2420c1e16
임해규 <limk9500@gmail.com>
2025-12-26 10:40:34 +0900

    init: Spring boot Setup

---
+++ backend/src/main/java/com/cookyuu/resume_chat/ResumeChatApplication.java
@@ -0,0 +1,13 @@
+package com.cookyuu.resume_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeChatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cookyuu/resume_chat/ResumeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookyuu.resume_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
